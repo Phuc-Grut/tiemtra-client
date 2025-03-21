0f689d163bd2fc0b0258c531f37661990cad0e96
PhucGrut <ngotronghl52@gmail.com>
2025-03-21 19:18:42 +0700

    up category, pagingation

--- src/views/Category/index.tsx
+++ src/views/Category/index.tsx
@@ -7,7 +7,7 @@ const***REMOVED***Category:***REMOVED***React.FC***REMOVED***=***REMOVED***()***
 ***REMOVED******REMOVED***return***REMOVED***(
 ***REMOVED******REMOVED******REMOVED******REMOVED***<Box***REMOVED***sx={{***REMOVED***padding:***REMOVED***3***REMOVED***}}>
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***<Typography***REMOVED***variant="h4"***REMOVED***sx={{***REMOVED***mb:***REMOVED***3***REMOVED***}}>Trang***REMOVED***Danh***REMOVED***Mục</Typography>
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***<Form***REMOVED***/>
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***{/****REMOVED***<Form***REMOVED***/>***REMOVED****/}
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***<CategoryTable***REMOVED***/>
 ***REMOVED******REMOVED******REMOVED******REMOVED***</Box>
 ***REMOVED******REMOVED***)
