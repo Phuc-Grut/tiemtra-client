850802bedbd0a7b3c3a9d6a2bc6f89f54e6868c3
PhucGrut <ngotronghl52@gmail.com>
2025-04-24 08:19:54 +0700

    up reponsive header

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -2,7 +2,7 @@ import***REMOVED***React***REMOVED***from***REMOVED***"react";
 
 const***REMOVED***HomePage***REMOVED***=***REMOVED***()***REMOVED***=>***REMOVED***{
 ***REMOVED******REMOVED***return***REMOVED***(
-***REMOVED******REMOVED******REMOVED******REMOVED***<div>Trang***REMOVED***Chủ</div>
+***REMOVED******REMOVED******REMOVED******REMOVED***<div***REMOVED***style={{backgroundColor:***REMOVED***"Highlight",***REMOVED***height:***REMOVED***"100vh"}}>Trang***REMOVED***Chủ</div>
 ***REMOVED******REMOVED***);
 };
 
