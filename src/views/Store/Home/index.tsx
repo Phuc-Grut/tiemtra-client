c69bcfa955c6c6fe8ef86a19c628f62f2b3a0036
PhucGrut <ngotronghl52@gmail.com>
2025-05-28 11:54:35 +0700

    up home store

--- src/views/Store/Home/index.tsx
+++ src/views/Store/Home/index.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+import IntroVideoSection from "./components/IntroVideoSection";
+import BannerSlideshowSection from "./components/BannerSlideshowSection";
 
 const HomePage = () => {
   return (
-    <div style={{backgroundColor: "Highlight", height: "100vh"}}>Trang Chủ</div>
+    <div style={{ backgroundColor: "#f9f9f9", minHeight: "100vh" }}>
+      <BannerSlideshowSection/>
+      <IntroVideoSection />
+    </div>
   );
 };
 
