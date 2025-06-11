40d9dfc9ec336aa598477b86f096df64c1019030
thuyngan-sketch <nt975981@gmail.com>
2025-06-11 23:25:30 +0700

    register

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -10,12 +10,16 @@ import StoreLayout from "src/layouts/StoreLayout";
 import Product from "src/views/Admin/Product";
 import ProtectedRoute from "src/components/ProtectedRoute";
 import ProductDetail from "src/views/Store/ProductDetail";
+import VerifyOtp from "src/views/Auth/pages/VerifyOtp";
 const AppRoutes = () => {
   return (
     <Routes>
       <Route element={<StoreLayout />}>
-        <Route path="/" element={<HomePage />} />
-        <Route path="san-pham/:code/:slug" element={<ProductDetail />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/verify-otp" element={<VerifyOtp />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<HomePage />} />
+      <Route path="san-pham/:code/:slug" element={<ProductDetail />} />
       </Route>
       <Route
         path="/admin"
@@ -31,8 +35,7 @@ const AppRoutes = () => {
         <Route path="attribute" element={<Attribute />} />
         <Route path="category/*" element={<Category />} />
       </Route>
-      <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<Login />} />
+      
     </Routes>
   );
 };
