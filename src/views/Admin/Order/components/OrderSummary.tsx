d2ec7ea4c3a561b8a5ba1d970d2e32a816edae8e
PhucGrut <ngotronghl52@gmail.com>
2025-09-19 14:59:50 +0700

    fix tiền áp dụng voucher

--- src/views/Admin/Order/components/OrderSummary.tsx
+++ src/views/Admin/Order/components/OrderSummary.tsx
@@ -1,25 +1,55 @@
 import { Box, Typography, Divider } from "@mui/material";
+import { AppliedVoucher } from "src/Interfaces/IOrder";
 
 const OrderSummary = ({
   totalItems,
-  totalAmount,
+  itemsSubtotal,
   shippingFee,
+  appliedVouchers = [],
 }: {
   totalItems: number;
-  totalAmount: number;
   shippingFee: number;
+  itemsSubtotal: number
+  appliedVouchers?: AppliedVoucher[];
 }) => {
-  const finalAmount = totalAmount + shippingFee;
+
+  const totalDiscount = appliedVouchers.reduce(
+    (sum, voucher) => sum + voucher.discountAmount,
+    0
+  )
+
+  const totalAmount = itemsSubtotal + shippingFee - totalDiscount
 
   return (
     <Box>
-      <Typography variant="h6" gutterBottom>Tổng kết</Typography>
+      <Typography variant="h6" gutterBottom>
+        Tổng kết
+      </Typography>
       <Typography>Số lượng sản phẩm: {totalItems}</Typography>
-      <Typography>Tiền hàng: {totalAmount.toLocaleString()}đ</Typography>
-      <Typography>Phí vận chuyển: {shippingFee.toLocaleString()}đ</Typography>
+
+      {/* Tiền hàng gốc */}
+      <Typography>Tiền hàng: {itemsSubtotal.toLocaleString()}₫</Typography>
+
+      {/* Phí vận chuyển */}
+      <Typography>Phí vận chuyển: {shippingFee.toLocaleString()}₫</Typography>
+
+      {/* Giảm giá */}
+      {totalDiscount > 0 && (
+        <>
+          {appliedVouchers.map((voucher, index) => (
+            <Typography key={index} color="red">
+              Giảm giá tiền hàng (mã {voucher.voucherCode}): -
+              {voucher.discountAmount.toLocaleString()}₫
+            </Typography>
+          ))}
+        </>
+      )}
+
       <Divider sx={{ my: 1 }} />
+
+      {/* Tổng cộng */}
       <Typography variant="h6" color="primary">
-        Tổng đơn: {finalAmount.toLocaleString()}đ
+        Tổng tiền: {totalAmount.toLocaleString()}₫
       </Typography>
     </Box>
   );
